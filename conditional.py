313cc1562407909077e2ab683f3fb1999c0ea348
Russell Osei-Adjei <russelladjei11@gmail.com>
2017-07-13 14:30:10 +0000

    Worked on a new conditional statement and added comments

--- conditional.py
+++ conditional.py
@@ -1,3 +1,4 @@
+#if condition
 i = 10
 if(i % 2):
     print "Odd Number"
@@ -5,14 +6,20 @@ else:
     print "Even Number"
     
 print '\n'
-    
-def isEven(numbers)
 
-def sums_evens (numbers):
+#Defining a function of even numbers    
+def isEven(numbers):
+    return number % 2 == 0
+isEven(3)
+
+def evens(numbers):
 	acc = 0
 	for i in (numbers):
 		if (i % 2 == 0):
 			acc = acc + i
-			return acc
+	return acc
+
+print evens([1, 6, 3, 7, 5, 8])
+print evens([76, 9, 43, 7])
 			
 
